fe6c575c691e583d97e172d3ee1fa0886d4a170b
Florian Ragwitz <rafl@debian.org>
2010-01-14 03:23:28 +0100

    Version 0.12.

--- lib/namespace/clean.pm
+++ lib/namespace/clean.pm
@@ -17,11 +17,11 @@ use Sub::Name qw(subname);
 
 =head1 VERSION
 
-0.11
+0.12
 
 =cut
 
-$VERSION         = '0.11';
+$VERSION         = '0.12';
 $STORAGE_VAR     = '__NAMESPACE_CLEAN_STORAGE';
 
 =head1 SYNOPSIS
